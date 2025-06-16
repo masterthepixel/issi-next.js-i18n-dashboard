7c33e97ba6b16a9fdf0c542affa2ff6ea51acf1d
masterthepixel <45719243+masterthepixel@users.noreply.github.com>
2025-06-16 17:18:44 -0400

    Update GlobeDemo.tsx

--- src/components/GlobeDemo.tsx
+++ src/components/GlobeDemo.tsx
@@ -393,9 +393,8 @@ export default function GlobeDemo() {
       color: colors[Math.floor(Math.random() * (colors.length - 1))],
     },
   ];
-
   return (
-    <div className="flex flex-row items-center justify-center py-20 h-screen md:h-auto dark:bg-black bg-white relative w-full">
+    <div className="flex flex-row items-center justify-center py-20 h-screen md:h-auto relative w-full">
       <div className="max-w-7xl mx-auto w-full relative overflow-hidden h-full md:h-[40rem] px-4">
         <motion.div
           initial={{
@@ -419,7 +418,6 @@ export default function GlobeDemo() {
             don&apos;t forget to share it. :)
           </p>
         </motion.div>
-        <div className="absolute w-full bottom-0 inset-x-0 h-40 bg-gradient-to-b pointer-events-none select-none from-transparent dark:to-black to-white z-40" />
         <div className="absolute w-full -bottom-20 h-72 md:h-full z-10">
           <World data={sampleArcs} globeConfig={globeConfig} />
         </div>
